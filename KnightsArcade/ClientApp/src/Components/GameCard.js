47bf3391d105f6861a965f17107472ebac1028f3
Drew <drewc273@gmail.com>
2019-03-15 03:40:47 -0400

    Games page is basically done, just need to add filtering which will be quick

--- KnightsArcade/ClientApp/src/Components/GameCard.js
+++ KnightsArcade/ClientApp/src/Components/GameCard.js
@@ -1,46 +1,102 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, Modal, Button, FormGroup, FormControl, ControlLabel, NavDropdown, MenuItem, Panel, Grid, Row, Col, Image} from 'react-bootstrap';
+import { Navbar, Nav, NavItem, Modal, Button, FormGroup, FormControl, ControlLabel, NavDropdown, MenuItem, Panel, Grid, Row, Col, Image, Popover, OverlayTrigger} from 'react-bootstrap';
 import './GameCard.css';
 import { Auth } from 'aws-amplify';
+import { Storage } from 'aws-amplify';
 
 export default class GameCard extends Component {
 	constructor(props) {
 		super(props);
 
 	    this.state = {
-	      
+	    	imgUrl: ""
 	    };
 	}
 
 	componentDidMount() {
 
-		Auth.currentAuthenticatedUser({
-		    bypassCache: false
-		}).then(user => {
-			this.setState({ loggedIn: true });
-			this.setState({ username: user.username});
-		})
-		.catch(err => {
-			console.log(err);
-			this.setState({ loggedIn: false });
-		});
+		const imgPath = this.props.gameData.gameImg[0];
+		Storage.get(imgPath)
+		    .then(result => {
+		    	this.setState({
+		    		imgUrl: result
+		    	});
+		    })
+		    .catch(err => console.log(err));
 
 	}
 
 	render(props) {
+		
+		const gameData = this.props.gameData;
+		const options = { year: 'numeric', month: 'long', day: 'numeric' };
+		const date = new Date(gameData.gameSubmissionDateUtc);
+		const genres = [];
+
+		if(gameData.gameGenreAction == true) {
+			genres.push("Action");
+		}
+		if(gameData.gameGenreAdventure == true) {
+			genres.push("Adventure");
+		}
+		if(gameData.gameGenreFighting == true) {
+			genres.push("Fighting");
+		}
+		if(gameData.gameGenrePlatformer == true) {
+			genres.push("Platformer");
+		}
+		if(gameData.gameGenrePuzzle == true) {
+			genres.push("Puzzle");
+		}
+		if(gameData.gameGenreRPG == true) {
+			genres.push("RPG");
+		}
+		if(gameData.gameGenreRacing == true) {
+			genres.push("Racing");
+		}
+		if(gameData.gameGenreRhythm == true) {
+			genres.push("Rhythm");
+		}
+		if(gameData.gameGenreShooter == true) {
+			genres.push("Shooter");
+		}
+		if(gameData.gameGenreSports == true) {
+			genres.push("Sports");
+		}
+		if(gameData.gameGenreStrategy == true) {
+			genres.push("Strategy");
+		}
+		if(gameData.gameGenreSurvival == true) {
+			genres.push("Survival");
+		}
+
+		const genreList = genres.join(", ");
+
+		const popoverHoverFocus = (
+		  <Popover id="popover-trigger-hover-focus" title={gameData.gameName}>
+		  	{gameData.gameDescription}
+		  </Popover>
+		);
+
 		return (
 			
+			<OverlayTrigger
+		      trigger={['hover', 'focus']}
+		      placement="right"
+		      overlay={popoverHoverFocus}
+		    >
 			<div className="game-card__card">
-				<Image width={240} height={135} src={require('../slideTest.png')} />
+				<Image width={240} height={135} src={this.state.imgUrl} />
 				<div className="game-card__info-container">
 					<div className="game-card__info-row-1">
-						<h3 className="game-card__game-title">Sushi Slap</h3>
-						<p className="game-card__dev">Developer: Drew</p>
+						<h3 className="game-card__game-title">{gameData.gameName}</h3>
+						<p className="game-card__dev">Developer: {gameData.gameCreatorName}</p>
 					</div>
-					<p>Released: Mar 14, 2019</p>
-					<p>puzzle, platformer, RPG</p>
+					<p>Released: {date.toLocaleDateString("en-US", options)}</p>
+					<p>{genreList}</p>
 				</div>
 			</div>
+			</OverlayTrigger>
 
 		);
 	}
--- KnightsArcade/ClientApp/src/Games.js
+++ KnightsArcade/ClientApp/src/Games.js
@@ -1,33 +1,221 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import NaviBar from './Components/NavBar';
+import GameCard from './Components/GameCard';
 import './Games.css';
-import {Panel, Grid, Row, Col, FormGroup, FormControl, InputGroup, DropdownButton, MenuItem, Form, ControlLabel} from 'react-bootstrap';
+import {Panel, Grid, Row, Col, FormGroup, FormControl, InputGroup, DropdownButton, MenuItem, Form, ControlLabel, Jumbotron} from 'react-bootstrap';
 
 class Games extends Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.handleGenreSelect = this.handleGenreSelect.bind(this);
+
+    this.state = {
+      games: [],
+      currentGenre: "All Genres",
+      AllGenres: true,
+      Action: false,
+      Adventure: false,
+      Fighting: false,
+      Platformer: false,
+      Puzzle: false,
+      RPG: false,
+      Racing: false,
+      Rhythm: false,
+      Shooter: false,
+      Sports: false,
+      Strategy: false,
+      Survival: false,
+    };
+  }
+
+  componentDidMount() {
+
+    axios.get(`api/v1/Public/rds/games/allgames`)
+      .then(res => {
+        const games = res.data;
+        this.setState({ games: games });
+      })
+
+  }
+
+  handleGenreSelect(eventKey) {
+    
+    this.setState({
+      AllGenres: false,
+      Action: false,
+      Adventure: false,
+      Fighting: false,
+      Platformer: false,
+      Puzzle: false,
+      RPG: false,
+      Racing: false,
+      Rhythm: false,
+      Shooter: false,
+      Sports: false,
+      Strategy: false,
+      Survival: false,
+    });
+
+    switch(eventKey) {
+      case 0:
+        this.setState({
+          AllGenres: true,
+          currentGenre: "All Genres"
+        });
+        break;
+      case 1:
+        this.setState({
+          Action: true,
+          currentGenre: "Action"
+        });
+        break;
+      case 2:
+        this.setState({
+          Adventure: true,
+          currentGenre: "Adventure"
+        });
+        break;
+      case 3:
+        this.setState({
+          Fighting: true,
+          currentGenre: "Fighting"
+        });
+        break;
+      case 4:
+        this.setState({
+          Platformer: true,
+          currentGenre: "Platformer"
+        });
+        break;
+      case 5:
+        this.setState({
+          Puzzle: true,
+          currentGenre: "Puzzle"
+        });
+        break;
+      case 6:
+        this.setState({
+          RPG: true,
+          currentGenre: "RPG"
+        });
+        break;
+      case 7:
+        this.setState({
+          Racing: true,
+          currentGenre: "Racing"
+        });
+        break;
+      case 8:
+        this.setState({
+          Rhythm: true,
+          currentGenre: "Rhythm"
+        });
+        break;
+      case 9:
+        this.setState({
+          Shooter: true,
+          currentGenre: "Shooter"
+        });
+        break;
+      case 10:
+        this.setState({
+          Sports: true,
+          currentGenre: "Sports"
+        });
+        break;
+      case 11:
+        this.setState({
+          Strategy: true,
+          currentGenre: "Strategy"
+        });
+        break;
+      case 12:
+        this.setState({
+          Survival: true,
+          currentGenre: "Survival"
+        });
+        break;
+    }
+  }
+
   render() {
     return (
+      <div className="games-page">
+      
+      <NaviBar/>
+      
+      <Jumbotron style={{marginBottom: 10, marginTop: 10}}>
+        <Grid fluid>
+        <Row>
+        <Col sm={6} smOffset={3}>
+        <h1>Game Library</h1>
+        <p>
+          Explore the official game library of KnightsArcade. Filled only with games made by proud UCF Knights.
+        </p>
+        </Col>
+        </Row>
+        </Grid>
+      </Jumbotron>
+      
+      <Grid fluid>
+      <Row>
+      <Col sm={6} smOffset={3}>
       <div className="games-page__form-container">
+        
         <div className="games-page__search-row">
-            <InputGroup style={{flexGrow: 1}}>
-              <FormControl type="text"/>
-              <DropdownButton
-                componentClass={InputGroup.Button}
-                id="input-dropdown-addon"
-                title="Action"
-              >
-                <MenuItem key="1">Item</MenuItem>
-              </DropdownButton>
-            </InputGroup>
-          <div className="games-page__sort">
-          <p style={{width: 50}}>Sort by</p>
-          <FormGroup>
-            <FormControl componentClass="select" placeholder="select">
-              <option value="select">select</option>
-              <option value="other">...</option>
-            </FormControl>
-          </FormGroup>
-          </div>
+          <Form style={{flexGrow: 1, alignSelf: "flex-start"}}>
+            <FormGroup style={{marginBottom: 0}}>
+              <InputGroup>
+                <FormControl type="text" placeholder="Search"/>
+                <DropdownButton
+                  componentClass={InputGroup.Button}
+                  id="input-dropdown-addon"
+                  title={this.state.currentGenre}
+                >
+                  <MenuItem eventKey={0} onSelect={this.handleGenreSelect} disabled={this.state.AllGenres}>All Genres</MenuItem>
+                  <MenuItem eventKey={1} onSelect={this.handleGenreSelect} disabled={this.state.Action}>Action</MenuItem>
+                  <MenuItem eventKey={2} onSelect={this.handleGenreSelect} disabled={this.state.Adventure}>Adventure</MenuItem>
+                  <MenuItem eventKey={3} onSelect={this.handleGenreSelect} disabled={this.state.Fighting}>Fighting</MenuItem>
+                  <MenuItem eventKey={4} onSelect={this.handleGenreSelect} disabled={this.state.Platformer}>Platformer</MenuItem>
+                  <MenuItem eventKey={5} onSelect={this.handleGenreSelect} disabled={this.state.Puzzle}>Puzzle</MenuItem>
+                  <MenuItem eventKey={6} onSelect={this.handleGenreSelect} disabled={this.state.RPG}>RPG</MenuItem>
+                  <MenuItem eventKey={7} onSelect={this.handleGenreSelect} disabled={this.state.Racing}>Racing</MenuItem>
+                  <MenuItem eventKey={8} onSelect={this.handleGenreSelect} disabled={this.state.Rhythm}>Rhythm</MenuItem>
+                  <MenuItem eventKey={9} onSelect={this.handleGenreSelect} disabled={this.state.Shooter}>Shooter</MenuItem>
+                  <MenuItem eventKey={10} onSelect={this.handleGenreSelect} disabled={this.state.Sports}>Sports</MenuItem>
+                  <MenuItem eventKey={11} onSelect={this.handleGenreSelect} disabled={this.state.Strategy}>Strategy</MenuItem>
+                  <MenuItem eventKey={12} onSelect={this.handleGenreSelect} disabled={this.state.Survival}>Survival</MenuItem>
+                </DropdownButton>
+              </InputGroup>
+            </FormGroup>
+          </Form>
+          <p style={{marginBottom:0, fontWeight: "bold", marginLeft: 20, marginRight: 15}}>Sort by</p>
+          <Form>
+            <FormGroup style={{marginBottom: 0}}>
+              <FormControl componentClass="select" placeholder="select">
+                <option value="select">Game Title</option>
+                <option value="other">Developer Name</option>
+                <option value="other">Release Date</option>
+              </FormControl>
+            </FormGroup>
+          </Form>
         </div>
+        
+        <div className="games-page__game-list">
+          {
+            this.state.games.map((game) => {
+              return <GameCard gameData={game} /> 
+              })
+          }
+        </div>
+
+      </div>
+      </Col>
+      </Row>
+      </Grid>
       </div>
     )
   }
