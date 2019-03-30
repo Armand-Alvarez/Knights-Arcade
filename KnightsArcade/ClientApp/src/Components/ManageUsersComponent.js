5b5f468374c1717d584f5db08ddd65820ae75796
Armand Alvarez <aalvarez97@knights.ucf.edu>
2019-03-29 22:24:39 -0400

    Added Admin functionality for managing users, updated manage games component. TODO: add functionality to manage users tab

--- KnightsArcade/ClientApp/src/Components/ManageUsersComponent.js
+++ KnightsArcade/ClientApp/src/Components/ManageUsersComponent.js
@@ -1,41 +1,72 @@
 import React, { Component } from 'react'
-import { Panel, Button, ButtonToolbar, Label } from 'react-bootstrap';
+import { Panel, Button, ButtonToolbar, Label, Image } from 'react-bootstrap';
 import axios from 'axios'
+import { Storage } from 'aws-amplify';
 
 
 export class ManageUsersComponent extends Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props);
     this.state = {
-      username: ''
-    }
-
-    this.handleClick = this.handleClick.bind(this)
+      imgName: '',
+      imgUrl: ''
+    };
   }
 
-  handleClick () {
-    axios.get('http://localhost:5002/api/v1/infrastructure/info')
-      .then(response => this.setState({username: response.data}))
+  componentDidMount() {
+    const imgPath = this.props.userData.userImagePath;
+    Storage.get(imgPath)
+      .then(result => {
+          this.setState({
+              imgName: this.props.userData.userImg,
+              imgUrl: result
+          });
+      })
+      .catch(err => console.log(err));
     }
 
+  // todo: add delete to swagger
+  handleDelete() {
+  }
+
+  // todo: add promote to swagger
+  handlePromote() {
+  }
+
   render() {
+    
+    const username = this.props.userData.username;
+    const firstName = this.props.userData.userFirstName;
+    const lastName = this.props.userData.userLastName;
+
+    console.log("username " + username);
+    console.log("firstName " + firstName);
+
     return (
       <div>
         <Panel>
+          <Panel.Heading><p>{username}</p><p>{firstName} {lastName}</p></Panel.Heading>
             <Panel.Body>
-                {/*Link to the user profile page. */}
-                <a href="#">Username Link</a>
+              <Image width={240} height={135} src={this.state.imgUrl}/>
+              <p>Major: {this.props.userData.userMajor}</p>
+              <p></p>
 
-                <ButtonToolbar>
-                    {/* Button allows Admin to edit User's name/major/email */}
-                    <Button bsStyle="success">Edit</Button>
+              <ButtonToolbar>
+              {/* Button allows Admin to promote a user to admin */}
+              <Button 
+                onClick={this.handlePromote}
+                bsStyle="success">
+                Promote To Admin
+              </Button>
 
+              {/* Button allows Admin to delete a user */}
+              <Button 
+                onClick={this.handleDelete}
+                bsStyle="danger">
+                Delete
+              </Button>
                     
         <div className='button__container'>
-      <button className='button' onClick={this.handleClick}>
-        Click Me
-      </button>
-      <p>{this.state.username}</p>
     </div>
                 </ButtonToolbar>
             </Panel.Body>
