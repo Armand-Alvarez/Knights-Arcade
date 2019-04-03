47b77a4379948af22ede3d0c78624cf418cd0987
Scott Kramarz <sscooter52@gmail.com>
2019-04-03 18:42:06 -0400

    Created Footer (Still in testing, only on review page atm)

--- KnightsArcade/ClientApp/src/Components/Footer.js
+++ KnightsArcade/ClientApp/src/Components/Footer.js
@@ -1,5 +1,5 @@
 ﻿import React, { Component } from 'react';
-import { Grid, Row, Col, Jumbotron } from 'react-bootstrap';
+import { Grid, Row, Col, Jumbotron, Image } from 'react-bootstrap';
 import './Footer.css';
 
 export default class Footer extends Component {
@@ -13,11 +13,14 @@ export default class Footer extends Component {
         return (
             <Grid className="Footer" fluid>
                 <Row style={{ marginLeft: 0, marginRight: 0, marginTop: 10, marginBottom: 10 }}>
-                            <Col md={2} mdOffset={2} style={{ paddingLeft: 0, paddingRight: 0 }}>
-                                <h2 className="FooterTitle">Footer</h2>
-                            </Col>
-                        </Row>
-                    </Grid>
-           );
+                    <Col md={1} mdOffset={2} style={{ paddingLeft: 0, paddingRight: 0 }}>
+                        <Image width={50} height={50} src={require("../Images/knightsicon.ico")} />
+                    </Col>
+                    <Col md={4} className="FooterText">
+                        <p>Knights Arcade: A University Of Central Florida Senior Design Project.</p>
+                    </Col>
+                </Row>
+            </Grid>
+        );
     }
-}
+}   
