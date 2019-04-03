1518fb6fa4692984b3ba71e3f850750db637b029
Scott Kramarz <sscooter52@gmail.com>
2019-04-03 14:10:25 -0400

    Added footer to help with page length to fill out pages, currently only on review page.

---
+++ KnightsArcade/ClientApp/src/Components/Footer.js
@@ -0,0 +1,23 @@
+﻿import React, { Component } from 'react';
+import { Grid, Row, Col, Jumbotron } from 'react-bootstrap';
+import './Footer.css';
+
+export default class Footer extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render(props) {
+
+        return (
+            <Grid className="Footer" fluid>
+                <Row style={{ marginLeft: 0, marginRight: 0, marginTop: 10, marginBottom: 10 }}>
+                            <Col md={2} mdOffset={2} style={{ paddingLeft: 0, paddingRight: 0 }}>
+                                <h2 className="FooterTitle">Footer</h2>
+                            </Col>
+                        </Row>
+                    </Grid>
+           );
+    }
+}
