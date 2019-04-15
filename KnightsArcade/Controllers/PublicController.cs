4a646abcd20b066cbcd35f320d899673b4c19941
Yitzak Hernandez <kre3ed@gmail.com>
2019-04-15 12:27:15 -0400

    Added email functionality to api. Removed automated controller (was never used).

--- KnightsArcade/Controllers/PublicController.cs
+++ KnightsArcade/Controllers/PublicController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Amazon.EC2;
 using Amazon.EC2.Model;
